fd81f91d65113f3bf0f684b3326e657493c3538c
Ítalo Andrade <andraditalo@gmail.com>
2018-09-13 00:44:13 -0300

    Adding more tests

--- api/core/user/user.test.js
+++ api/core/user/user.test.js
@@ -4,9 +4,11 @@ const
 
 require('chai').should();
 
+let scope = {};
+
 describe('User', () => {
     describe('POST - Sign in', () => {
-        it('should return user\'s info and token', async () => {
+        it('should return user\'s info and token when using credentials', async () => {
             const res = await request({
                 method: 'post',
                 uri: `${config.url}/user/sign-in`,
@@ -18,6 +20,67 @@ describe('User', () => {
             });
             res.user.should.be.not.null;
             res.token.should.be.not.null;
-        })
+            scope.token = res.token;
+        });
+
+        it('should return user\'s info and token when using token', async () => {
+            const res = await request({
+                method: 'post',
+                uri: `${config.url}/user/sign-in`,
+                headers: {
+                    'Authentication': scope.token
+                },
+                json: true
+            });
+            res.user.should.be.not.null;
+            res.token.should.be.not.null;
+        });
+
+        it('should return error code 1 (Usuário não encontrado)', async () => {
+            try {
+                await request({
+                    method: 'post',
+                    uri: `${config.url}/user/sign-in`,
+                    body: {
+                        email: 'test2@test.com',
+                        password: 'test'
+                    },
+                    json: true
+                });
+            } catch (res) {
+                res.error.code.should.be.equal(1);
+            }
+        });
+
+        it('should return error code 2 (Senha incorreta)', async () => {
+            try {
+                await request({
+                    method: 'post',
+                    uri: `${config.url}/user/sign-in`,
+                    body: {
+                        email: 'test@test.com',
+                        password: 'test2'
+                    },
+                    json: true
+                });
+            } catch (res) {
+                res.error.code.should.be.equal(2);
+            }
+        });
+
+        it('should return error code 3 (Token inválido)', async () => {
+            try {
+                await request({
+                    method: 'post',
+                    uri: `${config.url}/user/sign-in`,
+                    headers: {
+                        'Authentication': 'INVALIDTOKEN'
+                    },
+                    json: true
+                });
+            } catch (res) {
+                res.error.code.should.be.equal(3);
+            }
+        });
     })
 });
--- test/test.js
+++ test/test.js
@@ -2,4 +2,5 @@ describe('[Mocha]: Testing Boneare Adm API', async () => {
     require('../server')();
 
     require('../api/core/ping/ping.test');
+    require('../api/core/user/user.test');
 });
