d45a84b7c8419c944ac32ca4d832e2e6030d9e67
Ítalo Andrade <andraditalo@gmail.com>
2018-09-14 00:32:38 -0300

    Fix to not return a message from an internal error; Implementing debug on the response;

--- api/utils/asyncMiddleware.js
+++ api/utils/asyncMiddleware.js
@@ -1,3 +1,5 @@
+const config = require('../config');
+
 const defaultMessages = {
     404: 'Recurso não foi encontrado',
     500: 'Ocorreu um erro interno'
@@ -8,11 +10,17 @@ module.exports = fn =>
         Promise.resolve(fn(req, res, next))
             .then(() => next())
             .catch(err => {
+                if (err.constructor !== Object) {
+                    err = {
+                        err
+                    }
+                }
                 const httpCode = err.httpCode || 500;
                 const message = err.message || defaultMessages[httpCode];
                 const code = err.code;
                 res.error = err;
-                res.status(httpCode).send({message, code});
+                const debugError = config.debug ? err.err.toString() : undefined;
+                res.status(httpCode).send({message, code, debugError});
                 next()
             })
     };
