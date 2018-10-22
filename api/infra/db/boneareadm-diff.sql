fb68a34164c65031eeeb4ec3efeab9ba746fe552
Ítalo Andrade <andraditalo@gmail.com>
2018-10-22 18:13:35 -0300

    New tables client_phone and client_email

--- api/infra/db/boneareadm-diff.sql
+++ api/infra/db/boneareadm-diff.sql
@@ -1,32 +1,18 @@
 -- Diff code generated with pgModeler (PostgreSQL Database Modeler)
 -- pgModeler version: 0.9.2-alpha
--- Diff date: 2018-10-22 13:50:23
+-- Diff date: 2018-10-22 18:12:07
 -- Source model: boneareadm
 -- Database: boneareadm
 -- PostgreSQL version: 9.6
 
 -- [ Diff summary ]
 -- Dropped objects: 0
--- Created objects: 1
+-- Created objects: 0
 -- Changed objects: 1
 -- Truncated tables: 0
 
 SET search_path=public,pg_catalog,boneareadm;
 -- ddl-end --
-
-
--- [ Dropped objects ] --
-ALTER TABLE boneareadm.client_address DROP COLUMN IF EXISTS zipcode CASCADE;
--- ddl-end --
-
-
--- [ Created objects ] --
--- object: zip_code | type: COLUMN --
--- ALTER TABLE boneareadm.client_address DROP COLUMN IF EXISTS zip_code CASCADE;
-ALTER TABLE boneareadm.client_address ADD COLUMN zip_code varchar(8);
--- ddl-end --
-
-
 
 
 -- [ Changed objects ] --
