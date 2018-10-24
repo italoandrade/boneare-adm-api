87e39bf51d86ab960cb7e6d4483c3336a8f1adc1
Ítalo Andrade <andraditalo@gmail.com>
2018-10-24 02:36:19 -0300

    Fix: Init db only on server

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const
     cors = require('cors'),
     app = express(),
     config = require('./api/config'),
-    log = require('./api/utils/logHelper'),
-    db = require('./api/infra/db/db');
+    log = require('./api/utils/logHelper');
 
 module.exports = async () => {
-    global.db = db;
+    global.db = require('./api/infra/db/db');
 
     app.use(bodyParser.json());
     app.use(cors({
