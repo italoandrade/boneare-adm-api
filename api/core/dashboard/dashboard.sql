5aaf35b18dd9b2ed53c9e35910357a9c3125975c
Ítalo Andrade <andraditalo@gmail.com>
2018-10-26 17:26:31 -0300

    Change "value" to "amount"

--- api/core/dashboard/dashboard.sql
+++ api/core/dashboard/dashboard.sql
@@ -23,11 +23,11 @@ SELECT * FROM BoneareAdm.DashboardGetInfo();
 
 BEGIN
     RETURN QUERY
-    SELECT (SELECT COUNT(1) FROM BoneareAdm.Client)                                                      clients,
-           (SELECT COALESCE(SUM("value" * iif("type" = 1, 1, -1)), 0) FROM BoneareAdm.Order_Transaction) balance,
+    SELECT (SELECT COUNT(1) FROM BoneareAdm.Client)                                                     clients,
+           (SELECT COALESCE(SUM(amount * iif("type" = 1, 1, -1)), 0) FROM BoneareAdm.Order_Transaction) balance,
            (SELECT COALESCE(SUM((op."quantity" * iif(op."entry", 1, -1)) * p.weight), 0)
             FROM BoneareAdm.Order_Product op
-                     INNER JOIN BoneareAdm.Product p ON p.id = op.product_id)                            stock;
+                     INNER JOIN BoneareAdm.Product p ON p.id = op.product_id)                           stock;
 END;
 $$
 LANGUAGE plpgsql;
